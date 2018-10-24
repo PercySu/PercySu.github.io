e52e62a1365e4f8f678c432915425123d597fbeb
PercySu <hzpengcheng@gmail.com>
2018-10-24 23:07:42 +0800

    Site updated: 2018-10-24 23:07:41

--- dist/music.js
+++ dist/music.js
@@ -12,37 +12,37 @@ const ap = new APlayer({
         name: 'Swing,Swing',
         artist: 'The All-American Rejects',
         url: 'http://ox33lq08p.bkt.clouddn.com/Swing,%20Swing%20-%20The%20All-American%20Rejects.mp3',
-        cover: 'http://ox33lq08p.bkt.clouddn.com/aar.jpg'
+        cover: 'https://wx4.sinaimg.cn/mw1024/005HHyjJly1fwjpp3n3e6j30dw0dwmyr.jpg'
       },
       {
         name: '孤独的自由',
         artist: '玫瑰木子弹',
         url: 'http://ox33lq08p.bkt.clouddn.com/%E5%AD%A4%E7%8B%AC%E7%9A%84%E8%87%AA%E7%94%B1%20-%20%E6%9D%8E%E5%8D%9A_%E7%8E%AB%E7%91%B0%E6%9C%A8%E5%AD%90%E5%BC%B9.mp3',
-        cover: 'http://ox33lq08p.bkt.clouddn.com/%E7%8E%AB%E7%91%B0%E6%9C%A8%E5%AD%90%E5%BC%B9.jpg'
+        cover: 'https://wx3.sinaimg.cn/mw1024/005HHyjJly1fwjpoyxkswj30dw0dwgop.jpg'
       },
       {
         name: 'It Ends Tonight',
         artist: 'The All-American Rejects',
         url: 'http://ox33lq08p.bkt.clouddn.com/It%20Ends%20Tonight.mp3',
-        cover: 'http://ox33lq08p.bkt.clouddn.com/aar_1.jpg'
+        cover: 'https://wx2.sinaimg.cn/mw1024/005HHyjJly1fwjpp3iuh2j3046046t8v.jpg'
       },
       {
         name: 'Dirty Little Secrets',
         artist: 'The All-American Rejects',
         url: 'http://ox33lq08p.bkt.clouddn.com/Dirty%20Little%20Secret.mp3',
-        cover: 'http://ox33lq08p.bkt.clouddn.com/aar_movealong.jpg'
+        cover: 'https://wx1.sinaimg.cn/mw1024/005HHyjJly1fwjpp3r53qj30xc0x4acl.jpg'
       },
       {
         name: '玫瑰·子弹',
         artist: '玫瑰木子弹',
         url: 'http://ox33lq08p.bkt.clouddn.com/%E7%8E%AB%E7%91%B0%E2%96%AA%E5%AD%90%E5%BC%B9%20-%20%E6%9D%8E%E5%8D%9A_%E7%8E%AB%E7%91%B0%E6%9C%A8%E5%AD%90%E5%BC%B9.mp3',
-        cover: 'http://ox33lq08p.bkt.clouddn.com/%E7%8E%AB%E7%91%B0%E6%9C%A8%E5%AD%90%E5%BC%B9.jpg'
+        cover: 'https://wx3.sinaimg.cn/mw1024/005HHyjJly1fwjpoyxkswj30dw0dwgop.jpg'
       },
       {
         name: 'Days Killer',
         artist: 'Apple Pie',
         url: 'http://ox33lq08p.bkt.clouddn.com/%E8%8B%B9%E6%9E%9C%E6%B4%BE%E4%B9%90%E9%98%9F%20-%20%E6%97%B6%E5%85%89%E6%9D%80%E6%89%8B%EF%BC%88%E4%B8%AD%E6%96%87%EF%BC%89.mp3',
-        cover: 'http://ox33lq08p.bkt.clouddn.com/ApplePie.jpg'
+        cover: 'https://wx4.sinaimg.cn/mw1024/005HHyjJly1fwjpp431smj30m80m80yo.jpg'
       }
     ]
 });
