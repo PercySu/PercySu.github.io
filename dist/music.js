26c2ce95eece305d58c8181873365854373e2957
PercySu <hzpengcheng@gmail.com>
2018-10-25 15:34:58 +0800

    Site updated: 2018-10-25 15:34:57

--- dist/music.js
+++ dist/music.js
@@ -6,43 +6,43 @@ const ap = new APlayer({
         name: "21 Guns",
         artist: 'Green Day',
         url: 'https://music.163.com/song/media/outer/url?id=18127640.mp3',
-        cover: 'https://wx2.sinaimg.cn/mw690/005HHyjJly1fwjpauz6ryj30rs0rstwe.jpg'
+        cover: 'https://wx2.sinaimg.cn/mw1024/005HHyjJly1fwkgcn88b2j30rs0rstwe.jpg'
       },
       {
         name: 'Swing,Swing',
         artist: 'The All-American Rejects',
         url: 'https://music.163.com/song/media/outer/url?id=27138904.mp3',
-        cover: 'https://wx4.sinaimg.cn/mw690/005HHyjJly1fwjpp3n3e6j30dw0dwmyr.jpg'
+        cover: 'https://wx1.sinaimg.cn/mw1024/005HHyjJly1fwkgcm6bxqj30dw0dwmyr.jpg'
       },
       {
         name: '孤独的自由',
         artist: '玫瑰木子弹',
         url: 'https://music.163.com/song/media/outer/url?id=32334248.mp3',
-        cover: 'https://wx3.sinaimg.cn/mw690/005HHyjJly1fwjpoyxkswj30dw0dwgop.jpg'
+        cover: 'https://wx3.sinaimg.cn/mw1024/005HHyjJly1fwkgcm6ydsj30dw0dwgop.jpg'
       },
       {
         name: 'It Ends Tonight',
         artist: 'The All-American Rejects',
         url: 'https://music.163.com/song/media/outer/url?id=19594308.mp3',
-        cover: 'https://wx2.sinaimg.cn/mw690/005HHyjJly1fwjpp3iuh2j3046046t8v.jpg'
+        cover: 'https://wx3.sinaimg.cn/mw1024/005HHyjJly1fwkgcm0urqj3046046t8v.jpg'
       },
       {
         name: 'Dirty Little Secrets',
         artist: 'The All-American Rejects',
         url: 'https://music.163.com/song/media/outer/url?id=19594388.mp3',
-        cover: 'https://wx1.sinaimg.cn/mw690/005HHyjJly1fwjpp3r53qj30xc0x4acl.jpg'
+        cover: 'https://wx2.sinaimg.cn/mw1024/005HHyjJly1fwkgcm7no6j30xc0x4acl.jpg'
       },
       {
         name: '玫瑰·子弹',
         artist: '玫瑰木子弹',
         url: 'https://music.163.com/song/media/outer/url?id=32334247.mp3',
-        cover: 'https://wx3.sinaimg.cn/mw690/005HHyjJly1fwjpoyxkswj30dw0dwgop.jpg'
+        cover: 'https://wx3.sinaimg.cn/mw1024/005HHyjJly1fwkgcm6ydsj30dw0dwgop.jpg'
       },
       {
         name: 'Days Killer',
         artist: 'Apple Pie',
         url: 'https://music.163.com/song/media/outer/url?id=458082524.mp3',
-        cover: 'https://wx4.sinaimg.cn/mw690/005HHyjJly1fwjpp431smj30m80m80yo.jpg'
+        cover: 'https://wx1.sinaimg.cn/mw1024/005HHyjJly1fwkgcmbu8mj30m80m80yo.jpg'
       }
     ]
 });
