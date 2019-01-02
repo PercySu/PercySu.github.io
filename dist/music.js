a600833aacff0bcf635e97fe582fb7ab9379ef77
PercySu <hzpengcheng@gmail.com>
2019-01-02 18:48:41 +0800

    Site updated: 2019-01-02 18:48:39

--- dist/music.js
+++ dist/music.js
@@ -9,6 +9,12 @@ const ap = new APlayer({
         cover: 'https://wx2.sinaimg.cn/mw1024/005HHyjJly1fwkgcn88b2j30rs0rstwe.jpg'
       },
       {
+        name: "Wake Me Up When September Ends",
+        artist: 'Green Day',
+        url: 'https://music.163.com/song/media/outer/url?id=18127334.mp3',
+        cover: 'https://wx1.sinaimg.cn/mw690/005HHyjJly1fysfill3ykj30u00u041r.jpg'
+      },
+      {
         name: 'Swing,Swing',
         artist: 'The All-American Rejects',
         url: 'https://music.163.com/song/media/outer/url?id=27138904.mp3',
